01dfa20edecb08d905149757a81ca04dc7999ab3
HanneSixten <hanne.sixten@uib.no>
2023-05-30 10:20:31 +0200

    Double

--- Git and GitHub.R
+++ Git and GitHub.R
@@ -1,3 +1,9 @@
 library(usethis)
 
+library("palmerpenguins")
+data(penguins, package = "palmerpenguins")
+penguins
 
+library(ggplot2)
+ggplot(penguins, aes(x=bill_length_mm, y=bill_depth_mm)) + 
+  geom_point()
